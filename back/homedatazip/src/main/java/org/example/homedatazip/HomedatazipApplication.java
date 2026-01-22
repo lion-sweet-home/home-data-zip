2f3e96a81ad4477a3992eeb12e5777185e66cd5b
jeongbeomgyu <jeongbeomgyu@github.com>
2026-01-22 13:35:53 +0900

    [add] first

---
+++ back/homedatazip/src/main/java/org/example/homedatazip/HomedatazipApplication.java
@@ -0,0 +1,13 @@
+package org.example.homedatazip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomedatazipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomedatazipApplication.class, args);
+    }
+
+}
---
+++ back/homedatazip/src/test/java/org/example/homedatazip/HomedatazipApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.homedatazip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomedatazipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
