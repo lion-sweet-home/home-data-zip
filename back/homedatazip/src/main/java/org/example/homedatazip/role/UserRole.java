fda840aec4be795964416d71bf44bd4594a05bbc
jeongbeomgyu <jeongbeomgyu@github.com>
2026-01-26 20:47:16 +0900

    [add] UserRole

--- back/homedatazip/src/main/java/org/example/homedatazip/role/UserRole.java
+++ back/homedatazip/src/main/java/org/example/homedatazip/role/UserRole.java
@@ -1,4 +1,39 @@
 package org.example.homedatazip.role;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.homedatazip.user.entity.User;
+
+@Entity
+@Getter
+@Table(
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = {"user_id", "role_id"})
+        }
+)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id", nullable = false)
+    private Role role;
+
+    public static UserRole create(User user, Role role) {
+        UserRole userRole = new UserRole();
+        userRole.user = user;
+        userRole.role = role;
+
+        return userRole;
+    }
+
 }
