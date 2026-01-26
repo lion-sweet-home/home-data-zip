0ed44a6c2ddbd8003c958c9c8b8a594320962a13
jeongbeomgyu <jeongbeomgyu@github.com>
2026-01-26 20:47:04 +0900

    [add] Role

--- back/homedatazip/src/main/java/org/example/homedatazip/role/Role.java
+++ back/homedatazip/src/main/java/org/example/homedatazip/role/Role.java
@@ -1,4 +1,25 @@
 package org.example.homedatazip.role;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.homedatazip.role.type.RoleType;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(unique = true)
+    private RoleType roleType;
+
+    public Role(RoleType roleType) {
+        this.roleType = roleType;
+    }
 }
