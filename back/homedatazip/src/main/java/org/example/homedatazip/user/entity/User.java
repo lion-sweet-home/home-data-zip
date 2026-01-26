83a8a7be157b09111d093c66e888682c93e5df7e
jeongbeomgyu <jeongbeomgyu@github.com>
2026-01-26 20:49:02 +0900

    [add] User Entity

--- back/homedatazip/src/main/java/org/example/homedatazip/user/entity/User.java
+++ back/homedatazip/src/main/java/org/example/homedatazip/user/entity/User.java
@@ -1,12 +1,19 @@
 package org.example.homedatazip.user.entity;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.example.homedatazip.common.BaseTimeEntity;
+import org.example.homedatazip.role.Role;
+import org.example.homedatazip.role.UserRole;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User extends BaseTimeEntity {
 
     @Id
@@ -23,5 +30,21 @@ public class User extends BaseTimeEntity {
     private String password;
 
 
+    @OneToMany(
+            mappedBy = "user",
+            cascade = CascadeType.PERSIST,
+            orphanRemoval = true
+    )
+    private List<UserRole> roles = new ArrayList<>();
 
+    public static User create(String email, String nickname, String password, Role role) {
+        User user = new User();
+        user.email = email;
+        user.nickname = nickname;
+        user.password = password;
+        UserRole userRole = UserRole.create(user, role);
+        user.roles.add(userRole);
+
+        return user;
+    }
 }
