28687fe57c686a09bd18e5845aa70d8267dab7de
jaehyun <wogusrla77@naver.com>
2026-01-27 09:26:55 +0900

    [fix] UserSearchResponse class -> record로 변경

--- back/homedatazip/src/main/java/org/example/homedatazip/user/dto/UserSearchResponse.java
+++ back/homedatazip/src/main/java/org/example/homedatazip/user/dto/UserSearchResponse.java
@@ -1,22 +1,17 @@
 package org.example.homedatazip.user.dto;
 
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
 import org.example.homedatazip.user.entity.User;
 
 import java.time.LocalDateTime;
 
-@Getter
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder(access = AccessLevel.PRIVATE)
-public class UserSearchResponse {
-
-    private String nickname;
-    private String email;
-    private LocalDateTime createdAt;
-
+public record UserSearchResponse(
+        String nickname,
+        String email,
+        LocalDateTime createdAt
+) {
     public static UserSearchResponse create(User user) {
         return UserSearchResponse.builder()
                 .nickname(user.getNickname())
