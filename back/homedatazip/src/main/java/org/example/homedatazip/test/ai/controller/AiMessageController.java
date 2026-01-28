4273d4fe083e4347f3e455be21a47174a106022a
jeongbeomgyu <jeongbeomgyu@github.com>
2026-01-28 10:12:35 +0900

    [add] chatMessageController

---
+++ back/homedatazip/src/main/java/org/example/homedatazip/test/ai/controller/AiMessageController.java
@@ -0,0 +1,45 @@
+package org.example.homedatazip.test.ai.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.homedatazip.global.config.CustomUserDetails;
+import org.example.homedatazip.test.ai.dto.AiMessageRequest;
+import org.example.homedatazip.test.ai.entity.AiMessage;
+import org.example.homedatazip.test.ai.service.AiMessageService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/ai-chat")
+public class AiMessageController {
+    private final AiMessageService aiMessageService;
+
+    // 질문
+    @PostMapping("/ask")
+    public ResponseEntity<String> ask(
+            @RequestBody AiMessageRequest request,
+            @AuthenticationPrincipal CustomUserDetails userDetails
+    ) {
+        String answer = aiMessageService.askAndSave(userDetails.getUserId(), request.sessionId(), request.content());
+        return ResponseEntity.ok(answer);
+    }
+
+    // 특정 세션 대화 내역 조회
+    @GetMapping("/sessions/{sessionId}")
+    public ResponseEntity<List<AiMessage>> getChatMessage(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @PathVariable String sessionId) {
+        return ResponseEntity.ok().body(aiMessageService.getChatMessages(userDetails.getUserId(), sessionId));
+    }
+
+    // 세션 목록 조회
+    @GetMapping("/sessions")
+    public List<String> getSessionIds(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        return aiMessageService.getSessionIds(userDetails.getUserId());
+    }
+
+
+}
