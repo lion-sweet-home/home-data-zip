009589f21e1e0123e1478b90635f2ea08976366e
jeongbeomgyu <jeongbeomgyu@github.com>
2026-01-31 21:17:27 +0900

    [feat] record 에서 class 로 변경

--- back/homedatazip/src/main/java/org/example/homedatazip/tradeSale/dto/ApartmentTradeSaleItem.java
+++ back/homedatazip/src/main/java/org/example/homedatazip/tradeSale/dto/ApartmentTradeSaleItem.java
@@ -1,23 +1,33 @@
 package org.example.homedatazip.tradeSale.dto;
 
-public record ApartmentTradeSaleItem(
-        String sggCd, // 지역코드
-        String umdCd, // 법정동코드
-        String aptNm, // 아파트명
-        String jibun, // 지번
-        String excluUseAr, // 전용면적
-        String dealYear, // 계약년
-        String dealMonth, // 계약월
-        String dealDay, // 계약일
-        String dealAmount, // 거래금액
-        String floor, // 층
-        String buildYear, // 건축년도
-        String aptSeq, // 아파트 일련번호 (Unique Key)
-        String roadNm, // 도로명
-        String umdNm, // 법정동명
-        String aptDong, // 아파트 동
-        String cdealType, // 해제여부
-        String cdealDay, // 해제사유발생일
-        String roadNmBonbun,
-        String roadNmBubun
-) {}
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor // Jackson이 객체를 생성할 수 있게 해줌
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true) // 없는 필드는 무시해서 에러 방지
+public class ApartmentTradeSaleItem {
+
+    @JacksonXmlProperty(localName = "sggCd") private String sggCd;
+    @JacksonXmlProperty(localName = "umdCd") private String umdCd;
+    @JacksonXmlProperty(localName = "aptNm") private String aptNm;
+    @JacksonXmlProperty(localName = "jibun") private String jibun;
+    @JacksonXmlProperty(localName = "excluUseAr") private String excluUseAr;
+    @JacksonXmlProperty(localName = "dealYear") private String dealYear;
+    @JacksonXmlProperty(localName = "dealMonth") private String dealMonth;
+    @JacksonXmlProperty(localName = "dealDay") private String dealDay;
+    @JacksonXmlProperty(localName = "dealAmount") private String dealAmount;
+    @JacksonXmlProperty(localName = "floor") private String floor;
+    @JacksonXmlProperty(localName = "buildYear") private String buildYear;
+    @JacksonXmlProperty(localName = "aptSeq") private String aptSeq;
+    @JacksonXmlProperty(localName = "roadNm") private String roadNm;
+    @JacksonXmlProperty(localName = "umdNm") private String umdNm;
+    @JacksonXmlProperty(localName = "aptDong") private String aptDong;
+    @JacksonXmlProperty(localName = "cdealType") private String cdealType;
+    @JacksonXmlProperty(localName = "cdealDay") private String cdealDay;
+    @JacksonXmlProperty(localName = "roadNmBonbun") private String roadNmBonbun;
+    @JacksonXmlProperty(localName = "roadNmBubun") private String roadNmBubun;
+}
