dee6c70f739d749e43edb9a28db5f0b18f54f85c
jeongbeomgyu <jeongbeomgyu@github.com>
2026-02-01 14:54:12 +0900

    [faet] 단건 조회 & aptSeq 존재 확인 메소드 추가

--- back/homedatazip/src/main/java/org/example/homedatazip/apartment/repository/ApartmentRepository.java
+++ back/homedatazip/src/main/java/org/example/homedatazip/apartment/repository/ApartmentRepository.java
@@ -5,8 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 public interface ApartmentRepository extends JpaRepository<Apartment, Long> {
 
     List<Apartment> findAllByAptSeqIn(Collection<String> aptSeqs);
+
+    // [추가] 단건 조회를 위한 메서드 (중복 에러 방어용)
+    Optional<Apartment> findByAptSeq(String aptSeq);
+
+    // [추가] 존재 여부만 빠르게 확인하기 위한 메서드
+    boolean existsByAptSeq(String aptSeq);
 }
