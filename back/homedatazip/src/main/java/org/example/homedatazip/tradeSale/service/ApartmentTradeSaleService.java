759934147237d1068e9abeb07ab1adbc98498642
jeongbeomgyu <jeongbeomgyu@github.com>
2026-02-01 15:07:43 +0900

    [add] ApartmentTradeSaleService

---
+++ back/homedatazip/src/main/java/org/example/homedatazip/tradeSale/service/ApartmentTradeSaleService.java
@@ -0,0 +1,37 @@
+package org.example.homedatazip.tradeSale.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.homedatazip.apartment.entity.Apartment;
+import org.example.homedatazip.apartment.service.ApartmentService;
+import org.example.homedatazip.tradeSale.Repository.ApartmentTradeSaleRepository;
+import org.example.homedatazip.tradeSale.dto.ApartmentTradeSaleItem;
+import org.example.homedatazip.tradeSale.entity.TradeSale;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class ApartmentTradeSaleService {
+
+    private final ApartmentService apartmentService;
+    private final ApartmentTradeSaleRepository apartmentTradeSaleRepository;
+
+    @Transactional
+    public void processChunk(List<ApartmentTradeSaleItem> items) {
+        // 1. 아파트 정보 확보 (신규 저장 포함)
+        Map<String, Apartment> apartmentMap = apartmentService.getOrCreateApartmentsFromTradeSale(items);
+
+        // 2. 아파트 정보가 존재하는 아이템만 TradeSale로 변환
+        List<TradeSale> tradeSales = items.stream()
+                .filter(item -> apartmentMap.containsKey(item.getAptSeq())) // 아파트가 성공적으로 매칭된 것만!
+                .map(item -> TradeSale.from(item, apartmentMap.get(item.getAptSeq())))
+                .toList();
+
+        if (!tradeSales.isEmpty()) {
+            apartmentTradeSaleRepository.saveAll(tradeSales);
+        }
+    }
+}
