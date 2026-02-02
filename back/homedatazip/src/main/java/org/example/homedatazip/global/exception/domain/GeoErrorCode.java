2ec32874c89e13c052a8c5701318d3c0fc7c9a7f
jaehyun <wogusrla77@naver.com>
2026-02-02 14:12:51 +0900

    [feat] GeoErrorCode 에러코드 추가

--- back/homedatazip/src/main/java/org/example/homedatazip/global/exception/domain/GeoErrorCode.java
+++ back/homedatazip/src/main/java/org/example/homedatazip/global/exception/domain/GeoErrorCode.java
@@ -16,7 +16,13 @@ public enum GeoErrorCode implements ErrorCode {
     CONVERT_ADDRESS_ERROR(HttpStatus.NOT_FOUND,"GEO_404_1","좌표 변환 중 에러가 발생했습니다."),
 
     // 좌표 -> 주소 변환 중 에러
-    CONVERT_COORDINATE_ERROR(HttpStatus.NOT_FOUND, "GEO_404_2", "주소 변환 중 에러가 발생했습니다");
+    CONVERT_COORDINATE_ERROR(HttpStatus.NOT_FOUND, "GEO_404_2", "주소 변환 중 에러가 발생했습니다"),
+
+    INVALID_ADDRESS_FORMAT(HttpStatus.BAD_REQUEST, "GEO_400_0", "주소 형식이 올바르지 않아 변환이 불가능합니다."),
+
+    API_QUOTA_EXCEEDED(HttpStatus.TOO_MANY_REQUESTS, "GEO_429_0", "API 호출 횟수 제한을 초과했습니다."),
+
+    EXTERNAL_API_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "GEO_500_0", "외부 API 서버 통신 중 에러가 발생했습니다.");
 
     private final HttpStatus status;
     private final String code;
