415b40afaf03e02cf0f1b47a90c9cb001b51274d
jeongbeomgyu <jeongbeomgyu@github.com>
2026-02-03 13:38:08 +0900

    [feat] @QueryProjection 추가

--- back/homedatazip/src/main/java/org/example/homedatazip/apartment/dto/MarkResponse.java
+++ back/homedatazip/src/main/java/org/example/homedatazip/apartment/dto/MarkResponse.java
@@ -1,8 +1,18 @@
 package org.example.homedatazip.apartment.dto;
 
+import com.querydsl.core.annotations.QueryProjection;
+
 public record MarkResponse(
         Long aptId,
         String aptNm,
         Double latitude,
         Double longitude
-) {}
+) {
+    @QueryProjection
+    public MarkResponse(Long aptId, String aptNm, Double latitude, Double longitude) {
+        this.aptId = aptId;
+        this.aptNm = aptNm;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+}
