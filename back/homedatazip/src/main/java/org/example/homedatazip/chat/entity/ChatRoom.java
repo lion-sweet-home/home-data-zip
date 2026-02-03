83d66adc07ae7686538a7241aae042a6e69a647d
jaehyun <wogusrla77@naver.com>
2026-02-03 11:40:27 +0900

    [add] ChatRoom

---
+++ back/homedatazip/src/main/java/org/example/homedatazip/chat/entity/ChatRoom.java
@@ -0,0 +1,37 @@
+package org.example.homedatazip.chat.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.example.homedatazip.common.BaseTimeEntity;
+import org.example.homedatazip.listing.entity.Listing;
+import org.example.homedatazip.user.entity.User;
+
+@Entity
+@Getter
+@Table(name = "chat_rooms", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"buyer_id", "listing_id"})
+})
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder(access = AccessLevel.PRIVATE)
+public class ChatRoom extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "buyer_id", nullable = false)
+    private User buyer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "listing_id", nullable = false)
+    private Listing listing;
+
+    public static ChatRoom create(User buyer, Listing listing) {
+        return ChatRoom.builder()
+                .buyer(buyer)
+                .listing(listing)
+                .build();
+    }
+}
