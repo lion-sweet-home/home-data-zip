76e9a788dcd5711dc15fcdc20bde52c3faa84651
Ju-Hyun-J <jujung967@gmail.com>
2026-02-03 16:56:31 +0900

    [chore] 주석 추가

--- back/homedatazip/src/main/java/org/example/homedatazip/global/config/SecurityConfig.java
+++ back/homedatazip/src/main/java/org/example/homedatazip/global/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        // 로그인 사용자 전용 (me)
+                        // 로그인 사용자 전용 (favorite)
                         .requestMatchers("/api/users/me/**").authenticated()
                         // 회원가입·로그인 등
                         .requestMatchers("/api/users/**").permitAll()
