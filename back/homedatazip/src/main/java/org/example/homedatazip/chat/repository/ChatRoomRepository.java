b367198ecf7e3701f445d816a57cb93f775ea918
jaehyun <wogusrla77@naver.com>
2026-02-04 15:04:52 +0900

    [fix] ChatRoomRepository / ChatMessageRepository

--- back/homedatazip/src/main/java/org/example/homedatazip/chat/repository/ChatRoomRepository.java
+++ back/homedatazip/src/main/java/org/example/homedatazip/chat/repository/ChatRoomRepository.java
@@ -2,9 +2,24 @@ package org.example.homedatazip.chat.repository;
 
 import org.example.homedatazip.chat.entity.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
     Optional<ChatRoom> findByBuyerIdAndListingId(Long buyerId, Long listingId);
+
+    Optional<ChatRoom> findByListingId(Long listingId);
+
+    @Query("""
+        select count(cr) > 0
+        from ChatRoom cr
+        where cr.id = :roomId
+        and(
+            cr.buyer.email = :email
+            or cr.listing.user.email = :email
+        )
+""")
+    boolean existsParticipant(@Param("roomId") Long roomId, @Param("email") String email);
 }
