a80817be87959b4186732a70d4f9e5a5e14671d6
seoyoung <seoyoung.project@gmail.com>
2026-02-09 13:58:46 +0900

    save local changes

---
+++ back/homedatazip/src/main/java/org/example/homedatazip/apartment/entity/ApartmentSchoolDistance.java
@@ -0,0 +1,10 @@
+package org.example.homedatazip.apartment.entity;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Table(name = "apartment_")
+public class ApartmentSchoolDistance {
+}
