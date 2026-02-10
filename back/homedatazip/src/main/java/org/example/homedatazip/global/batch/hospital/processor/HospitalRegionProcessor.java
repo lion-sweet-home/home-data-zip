6517a054d2b1ca02ac8e760a81e414912952b0c1
Kim Jaehun <hun970412@naver.com>
2026-02-10 16:50:43 +0900

    [refactor] Region Cache 설정

--- back/homedatazip/src/main/java/org/example/homedatazip/global/batch/hospital/processor/HospitalRegionProcessor.java
+++ back/homedatazip/src/main/java/org/example/homedatazip/global/batch/hospital/processor/HospitalRegionProcessor.java
@@ -9,6 +9,9 @@ import org.example.homedatazip.hospital.entity.Hospital;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 @Slf4j
 @Component
 @RequiredArgsConstructor
@@ -16,23 +19,58 @@ public class HospitalRegionProcessor implements ItemProcessor<Hospital, Hospital
 
     private final GeoService geoService;
 
+    // 좌표를 반올림하여 Region Cache에 저장
+    private final Map<String, Region> regionCache = new ConcurrentHashMap<>();
+
     @Override
     public Hospital process(Hospital hospital) throws Exception {
-
-        try {
-            Region region = geoService.convertAddressInfoInNewTransaction(
+        if (hospital.getLatitude() == null || hospital.getLongitude() == null) {
+            log.warn("⏭️ 위경도 누락 (스킵) - Hospital Id: {}, Hospital: {}, 위도: {}, 경도: {}",
+                    hospital.getHospitalId(),
+                    hospital.getName(),
                     hospital.getLatitude(),
                     hospital.getLongitude()
             );
+            return null;
+        }
 
-            hospital.attachRegion(region);
-            return hospital;
-        } catch (BatchSkipException e) {
-            log.warn("⏭️ Region 매칭 실패 (스킵) - Hospital: {}, 사유: {}",
-                    hospital.getName(),
-                    e.getMessage()
+        // 좌표를 소수점 3자리로 반올림 (약 111m 정밀도)
+        String cacheKey = roundCoordinate(hospital.getLatitude(), hospital.getLongitude());
+
+        Region region = regionCache.computeIfAbsent(cacheKey, key -> {
+                    try {
+                        return geoService.convertAddressInfoInNewTransaction(
+                                hospital.getLatitude(),
+                                hospital.getLongitude()
+                        );
+                    } catch (BatchSkipException e) {
+                        log.warn("⏭️ Region 매칭 실패 (스킵) - Hospital Id: {}, Hospital: {}, 사유: {}",
+                                hospital.getHospitalId(),
+                                hospital.getName(),
+                                e.getMessage()
+                        );
+                        return null;
+                    }
+                }
+        );
+
+        if (region == null) {
+            log.warn("⏭️ Region 누락 - Hospital Id: {}, Hospital: {}",
+                    hospital.getHospitalId(),
+                    hospital.getName()
             );
             return null;
         }
+
+        hospital.attachRegion(region);
+        return hospital;
+    }
+
+    private String roundCoordinate(Double latitude, Double longitude) {
+        // 소수점 3자리 => 약 111m 반경
+        return String.format("%.3f, %.3f",
+                Math.round(latitude * 1000.0) / 1000.0,
+                Math.round(longitude * 1000.0) / 1000.0
+        );
     }
 }
