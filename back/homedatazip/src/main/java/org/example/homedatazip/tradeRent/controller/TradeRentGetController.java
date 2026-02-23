678dec32d90e4a688d97642c2b4fc316b6f00369
jeongbeomgyu <jeongbeomgyu@github.com>
2026-02-23 10:57:26 +0900

    [feat] 사용자 기반 로그 저장 Service 추가

--- back/homedatazip/src/main/java/org/example/homedatazip/tradeRent/controller/TradeRentGetController.java
+++ back/homedatazip/src/main/java/org/example/homedatazip/tradeRent/controller/TradeRentGetController.java
@@ -4,12 +4,18 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.example.homedatazip.apartment.dto.MarkResponse;
 import org.example.homedatazip.apartment.dto.MarkerClusterResponse;
+import org.example.homedatazip.global.config.CustomUserDetails;
+import org.example.homedatazip.recommend.service.SearchLogService;
+import org.example.homedatazip.recommend.type.LogType;
+import org.example.homedatazip.recommend.type.TradeType;
 import org.example.homedatazip.tradeRent.dto.DotResponse;
 import org.example.homedatazip.tradeRent.dto.RentFromAptResponse;
 import org.example.homedatazip.tradeRent.dto.RentGetMarkerRequest;
 import org.example.homedatazip.tradeRent.dto.detailList.RentDetailList5Response;
 import org.example.homedatazip.tradeRent.service.TradeRentGetService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,12 +27,18 @@ import java.util.List;
 public class TradeRentGetController {
 
     private final TradeRentGetService tradeRentGetService;
+    private final SearchLogService searchLogService;
 
     //좌표 마킹
     @GetMapping
     public ResponseEntity<List<MarkResponse>> markerRent(
-            @ModelAttribute @Valid RentGetMarkerRequest request
+            @ModelAttribute @Valid RentGetMarkerRequest request,
+            @AuthenticationPrincipal CustomUserDetails user
     ) {
+        if (user != null && user.getUserId() != null) {
+            searchLogService.saveRentSearchLog(user.getUserId(), request);
+        }
+
         return ResponseEntity.ok(tradeRentGetService.getListRentsByAptId(request));
     }
 
@@ -51,7 +63,12 @@ public class TradeRentGetController {
 
     //마크 선택시 하단 최근 거래내역
     @GetMapping("/{aptId}")
-    public ResponseEntity<List<RentFromAptResponse>> rentByAptId(@PathVariable Long aptId){
+    public ResponseEntity<List<RentFromAptResponse>> rentByAptId(@PathVariable Long aptId, @AuthenticationPrincipal CustomUserDetails user){
+
+        if (user != null) {
+            searchLogService.saveActionLog(user.getUserId(), aptId, LogType.SUMMARY, TradeType.RENT);
+        }
+
         List<RentFromAptResponse> rentLimit5 = tradeRentGetService.getRentLimit5(aptId);
         return ResponseEntity.ok(rentLimit5);
     }
@@ -60,7 +77,11 @@ public class TradeRentGetController {
     @GetMapping("/{aptId}/detail")
     public ResponseEntity<RentDetailList5Response> rentByAptDealDate(@PathVariable Long aptId,
                                                                      @RequestParam Long areaKey,
-                                                                     @RequestParam(name = "period", required = false) Integer period){
+                                                                     @RequestParam(name = "period", required = false) Integer period,@AuthenticationPrincipal CustomUserDetails user){
+        if (user != null) {
+            searchLogService.saveActionLog(user.getUserId(), aptId, LogType.DETAIL, TradeType.RENT);
+        }
+
         RentDetailList5Response rentAreaType = tradeRentGetService.getRentAreaType(aptId, areaKey, period);
         return ResponseEntity.ok(rentAreaType);
     }
