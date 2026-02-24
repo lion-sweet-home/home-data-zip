f9a2761e6a80669258845427704e6ad066eaea24
choiminhyeok <edum96@gmail.com>
2026-02-24 17:56:40 +0900

    그래프 면적분리 수정

--- back/homedatazip/src/main/java/org/example/homedatazip/tradeRent/dto/DotResponse.java
+++ back/homedatazip/src/main/java/org/example/homedatazip/tradeRent/dto/DotResponse.java
@@ -3,6 +3,7 @@ package org.example.homedatazip.tradeRent.dto;
 
 public record DotResponse(
         Long deposit,
+        Double exclusive,
         Integer mothlyRent,
         String yyyymm,
         Integer floor
